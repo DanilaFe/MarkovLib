f6851fd9fb1c2a77e4be3ee30be77434c915455d
Danila Fedorin <danila.fedorin@gmail.com>
2016-02-07 18:01:46 -0800

    Add startValue and endValue; Finalized training and chain generation.

--- src/com/danilafe/markovlib/ChainManager.java
+++ src/com/danilafe/markovlib/ChainManager.java
@@ -5,6 +5,13 @@ import java.util.ArrayList;
 public class ChainManager<T> {
 
 	public ArrayList<ChainElement<T>> containedElements = new ArrayList<ChainElement<T>>();
+	public T startvalue;
+	public T endvalue;
+
+	public ChainManager(T startelement, T endelement) {
+		this.startvalue = startelement;
+		this.endvalue = endelement;
+	}
 
 	public ChainElement<T> getChainElementFor(T t){
 		for(ChainElement<T> chainElement : containedElements)
@@ -12,8 +19,36 @@ public class ChainManager<T> {
 		return null;
 	}
 
-	public void train(T...ts){
+	public ChainElement<T> createChainElementFor(T t){
+		ChainElement<T> newElement = new ChainElement<T>(t);
+		containedElements.add(newElement);
+		return newElement;
+	}
 
+	public ChainElement<T> chainElementFor(T t){
+		ChainElement<T> element = null;
+		return ((element = getChainElementFor(t)) == null) ? createChainElementFor(t) : element;
+	}
+
+	public void train(T...ts){
+		for(int i = 0; i < ts.length; i++){
+			T previousValue = (i > 0) ? ts[i - 1] : startvalue;
+			ChainElement<T> previousElement = chainElementFor(previousValue);
+			previousElement.add(ts[i]);
+		}
+
+		if(ts.length > 0) chainElementFor(ts[ts.length - 1]).add(endvalue);
+	}
+
+	public ArrayList<T> generateChain() {
+		for(ChainElement<T> element : containedElements) element.update();
+
+		ArrayList<T> generatedValues = new ArrayList<T>();
+		for(T lastValue = startvalue; !(lastValue = chainElementFor(lastValue).getRandom()).equals(endvalue);) {
+			generatedValues.add(lastValue);
+		}
+
+		return generatedValues;
 	}
 
 }
